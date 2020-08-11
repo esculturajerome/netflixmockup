3594f21be9fde9bb3e0f7359a9ce5be659c16a9f
esculturajerome <esculturajerome@gmail.com>
2020-08-11 18:53:02 +0800

    Netflix | Fix the scrollable title in Row

--- src/components/Row.js
+++ src/components/Row.js
@@ -47,23 +47,27 @@ function Row({ title, fetchURL, isLargeRow }) {
   };
 
   return (
-    <div className="row">
-      <h2>{title}</h2>
-      <div className="row__posters">
-        {movies.map((movie) => (
-          <img
-            key={movie.id}
-            onClick={() => handleClick(movie)}
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            src={`${base_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-          />
-        ))}
+    <React.Fragment>
+      <div className="row">
+        <h2>{title}</h2>
       </div>
-      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
-    </div>
+      <div className="row">
+        <div className="row__posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              onClick={() => handleClick(movie)}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          ))}
+        </div>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
+    </React.Fragment>
   );
 }
 
